396afc36fca1208aadb7fe108d7d580b1168e3c0
Karlo Barjaktarić <62699643+Joker2001-bit@users.noreply.github.com>
2023-02-08 00:10:54 +0100

    menu+3 players
    Select a player menu added: a user can choose one of 3 players to play with, and high score is presented for each one of them after game over

--- main.c
+++ main.c
@@ -9,9 +9,13 @@
 uint8_t kom[10][2];
 uint16_t brojKometa;
 int brojSerija;
-int highScore = 0;
+int faze = 0;
+int isButtonClicked = 0;
+int highScore[3] = {0, 0, 0};
 uint8_t player_x;
 uint8_t player_y;
+int location;
+int startGame;
 
 void delay(uint16_t timeout) {
 	uint16_t i;
@@ -32,8 +36,82 @@ void initialize_first_screen(void) {
 	
 }
 
+void initialize_menu(void) {
+	PORTB = _BV(0) | _BV(1) | _BV(2);
+	DDRB = 0x00;
+	int flag = 0;
+	startGame = 0;
+	location = 0;
+	OLED_Init();
+	OLED_Clear();
+	while(!startGame){
+		if(isButtonClicked){
+			OLED_Clear();
+			isButtonClicked = 0;
+		}
+		OLED_SetCursor(1, 24);
+		OLED_Printf("Select a player:");
+		if(location == 0){
+			OLED_SetCursor(3, 29);
+			OLED_Printf("-> Player 1 <-");
+			OLED_SetCursor(4, 46);
+			OLED_Printf("Player 2");
+			OLED_SetCursor(5, 46);
+			OLED_Printf("Player 3");
+		} 
+		if(location == 1){
+			OLED_SetCursor(3, 46);
+			OLED_Printf("Player 1");
+			OLED_SetCursor(4, 29);
+			OLED_Printf("-> Player 2 <-");
+			OLED_SetCursor(5, 46);
+			OLED_Printf("Player 3");
+		}
+		if(location == 2){
+			OLED_SetCursor(3, 46);
+			OLED_Printf("Player 1");
+			OLED_SetCursor(4, 46);
+			OLED_Printf("Player 2");
+			OLED_SetCursor(5, 29);
+			OLED_Printf("-> Player 3 <-");
+		}
+		if(bit_is_clear(PINB,  0))  {
+			flag = 1;
+			isButtonClicked = 1;
+			delay(100);
+		}
+		if(bit_is_clear(PINB,  1))  {
+			flag = 2;
+			isButtonClicked = 1;
+			delay(100);
+		}
+		if(bit_is_clear(PINB,  2))  {
+			flag = 3;
+			delay(100);
+		}
+
+		switch(flag)  {
+			case 1:
+				if(location == 0) location = 0;
+				else location--;
+				flag = 0;
+				break;
+			case 2:
+				if(location == 2) location = 2;
+				else location++;
+				flag = 0;
+				break;
+			case 3: 
+				startGame = 1;
+				flag = 0;
+				break;
+		}
+
+	}
+	initialize_lcd();
+}
+
 void initialize_lcd(void) {
-	
 	OLED_Init();
 	OLED_Clear();
 	brojKometa = 1;
@@ -55,14 +133,14 @@ void draw_screen(uint8_t kom[][2], uint8_t px, uint8_t py) {
 }
 
 void game_over () {
-	if (brojSerija > highScore) highScore = brojSerija;
+	if (brojSerija > highScore[location]) highScore[location] = brojSerija;
 	OLED_Clear();
 	OLED_SetCursor(2, 36);
 	OLED_Printf("Game over");
 	OLED_SetCursor(6, 25);
-	OLED_Printf("High score: %d", highScore);
+	OLED_Printf("High score: %3d", highScore[location]);
 	_delay_ms(2000);
-	initialize_lcd();
+	initialize_menu();
 }
 
 ISR(INT0_vect) {
@@ -73,7 +151,6 @@ ISR(INT0_vect) {
 }
 
 ISR(INT1_vect) {
-
 	player_x += 5;
 	if(player_x >= 123) player_x = 122;
 
@@ -88,7 +165,6 @@ void debounce() {
 
 int main(void)
 {
-	
 	MCUCR = _BV(ISC01) | _BV(ISC11);
 	GICR = _BV(INT0) | _BV(INT1);
 	sei();
@@ -96,14 +172,15 @@ int main(void)
 	srand(time(NULL));
 	
 	initialize_first_screen();
-
-	initialize_lcd();
+	initialize_menu();
 
 	while (1) {
 		
 		for (uint8_t i = 0; i < brojKometa; i++)
 		{
-			if(kom[i][0] >= (player_x - 4) && kom[i][0] <= (player_x + 4) && (kom[i][1] == player_y)) game_over();
+			if(kom[i][0] >= (player_x - 4) && kom[i][0] <= (player_x + 4) && (kom[i][1] == player_y)){ 
+				game_over();
+			}
 		}
 
 		draw_screen(kom, player_x, player_y);
@@ -123,7 +200,9 @@ int main(void)
 				if(i > 0){
 					for (int j = i-1; j >= 0; j--)
 					{
-						if(kom[i][0] >= kom[j][0]-3 && kom[i][0] <= kom[j][0]+3) kom[i][0] = (kom[i][0] +10) % 122;
+						if(kom[i][0] >= kom[j][0]-3 && kom[i][0] <= kom[j][0]+3) kom[i][0] = (kom
+
+[i][0] +10) % 122;
 					}
 				}
 			}
